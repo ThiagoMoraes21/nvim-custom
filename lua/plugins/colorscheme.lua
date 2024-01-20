0fbd676a2a2daf92b84cdcbe1eee2c7ea1ea25a9
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-01-20 18:18:45 -0300

    add: theme configuration plugin

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  -- add solarized-osaka
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+
+  -- Configure LazyVim to load solarized-osaka
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+
+}
