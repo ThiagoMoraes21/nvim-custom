486247d01715ad4f2aec0334d8dd7382a9f573ac
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-01-25 15:50:24 -0300

    add: some extra lsp configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -47,6 +46,24 @@ require("lazy").setup({
         "tohtml",
         "tutor",
         "zipPlugin",
+      },
+    },
+  },
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
       },
     },
   },
